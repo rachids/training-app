f68fab7198de68160f18eb1956e9eb47c8e4f60d
rachids <livecoding@rachids.ca>
2021-08-12 13:45:25 -0400

    Add training date to TrainingResource

--- app/Http/Resources/TrainingResource.php
+++ app/Http/Resources/TrainingResource.php
@@ -17,6 +17,7 @@ class TrainingResource extends JsonResource
         return [
             'id' => $this->id,
             'repetitions' => $this->value,
+            'date' => $this->created_at,
             'exercice' => $this->whenLoaded('exercice'),
         ];
     }
